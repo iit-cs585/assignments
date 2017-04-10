3a1d30b39feef3eb336f614ee0fc59fdeb7b7b58
Aron <aronwc@gmail.com>
2017-04-10 16:48:11 -0500

    a4 posted

--- a4/README.md
+++ a4/README.md
@@ -1,1 +1,41 @@
-A4
+## Part 1
+
+In this part, we will train a word2vec model, then use it to (hopefully) improve the named-entity classifier from A3.
+You will have to modify a4.py to accomplish the instructions below. You may use your solution to A3 as a starting point.
+
+**Output should be written to output1.txt**
+
+1. Install [gensim](https://radimrehurek.com/gensim/) using `pip install gensim`.
+2. Install [nltk](http://www.nltk.org/) using `pip install nltk`.
+3. Download the Brown Corpus, a collection of news articles.
+  - From a python shell, do:
+  ```
+  import nltk
+  nltk.download('brown')
+  ```
+4. Read the word2vec gensim documentation [here](https://radimrehurek.com/gensim/models/word2vec.html)
+5. Train a word2vec model on the sentences from the Brown corpus.
+  - Use `size=50`, `window=5`, and `min_count=5`
+6. Modify your make_feature_dicts method from a3 to have:
+  - an additional parameter w2v_model, which will store the... word2vec model you trained.
+  - an additional parameter w2v, which is a boolean, that is True if we are to add the w2v features
+  - For each token in the input, if w2v==True, you will add 50 new feature from the word2vec model.
+    - E.g., one feature key may be 'w2v_6', and its value may be 2.6, indicating that the 6th w2v dimension has value 2.6 for this token.
+  - Allow the word2vec features to be combined with the context features as well (so, you could have a feature like 'prev_w2v_6' = 2.6.
+7. Modify evaluate_combinations to take in the word2vec model. Re-run all combinations and report the final results.
+8. Store the output of evaluate_combinations in a file called `output1.txt`.
+
+
+## Part 2: Bonus (15 pts)
+
+Use TensorFlow to implement a Recurrent Neural Network for the Named Entity Recognition problem.  
+**Your code will go in rnn.py. The output of your program should be written to output2.txt.**
+
+Using the same data as the above, train a Recurrent Neural Network.
+- Use dimension 20 for the hidden layer of each state
+- Use cross-entropy as the loss function
+- Use the logistic function as the activation function for the hidden state, and use softmax to product the output for each time step.
+- Other than that, you are free to use any parameter settings you like to optimize the model.
+
+You may find it helpful to go through the tensorflow tutorial here:
+https://www.tensorflow.org/tutorials/recurrent
---
+++ a4/a4.py
@@ -0,0 +1,169 @@
+# coding: utf-8
+"""CS585: Assignment 4
+
+See README.md
+"""
+
+###  You may add to these imports for gensim and nltk.
+from collections import Counter
+from itertools import product
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+import urllib.request
+#####################################
+
+
+def download_data():
+    """ Download labeled data.
+    DONE ALREADY.
+    """
+    url = 'https://www.dropbox.com/s/bqitsnhk911ndqs/train.txt?dl=1'
+    urllib.request.urlretrieve(url, 'train.txt')
+    url = 'https://www.dropbox.com/s/s4gdb9fjex2afxs/test.txt?dl=1'    
+    urllib.request.urlretrieve(url, 'test.txt')
+    
+    
+def read_data(filename):
+    """
+    Read the data file into a list of lists of tuples.
+    
+    Each sentence is a list of tuples.
+    Each tuple contains four entries:
+    - the token
+    - the part of speech
+    - the phrase chunking tag
+    - the named entity tag
+    
+    For example, the first two entries in the
+    returned result for 'train.txt' are:
+    
+    > train_data = read_data('train.txt')
+    > train_data[:2]
+    [[('EU', 'NNP', 'I-NP', 'I-ORG'),
+      ('rejects', 'VBZ', 'I-VP', 'O'),
+      ('German', 'JJ', 'I-NP', 'I-MISC'),
+      ('call', 'NN', 'I-NP', 'O'),
+      ('to', 'TO', 'I-VP', 'O'),
+      ('boycott', 'VB', 'I-VP', 'O'),
+      ('British', 'JJ', 'I-NP', 'I-MISC'),
+      ('lamb', 'NN', 'I-NP', 'O'),
+      ('.', '.', 'O', 'O')],
+     [('Peter', 'NNP', 'I-NP', 'I-PER'), ('Blackburn', 'NNP', 'I-NP', 'I-PER')]]
+    """
+    ###TODO
+    pass
+
+def make_feature_dicts(data,
+					   token=True,
+                       caps=True,
+                       pos=True,
+                       chunk=True,
+                       context=True):
+    """
+    Create feature dictionaries, one per token. Each entry in the dict consists of a key (a string)
+    and a value of 1.
+    Also returns a numpy array of NER tags (strings), one per token.
+
+    See a3_test.
+
+    The parameter flags determine which features to compute.
+    Params:
+    data.......the data returned by read_data
+    token......If True, create a feature with key 'tok=X', where X is the *lower case* string for this token.
+    caps.......If True, create a feature 'is_caps' that is 1 if this token begins with a capital letter.
+               If the token does not begin with a capital letter, do not add the feature.
+    pos........If True, add a feature 'pos=X', where X is the part of speech tag for this token.
+    chunk......If True, add a feature 'chunk=X', where X is the chunk tag for this token
+    context....If True, add features that combine all the features for the previous and subsequent token.
+               E.g., if the prior token has features 'is_caps' and 'tok=a', then the features for the
+               current token will be augmented with 'prev_is_caps' and 'prev_tok=a'.
+               Similarly, if the subsequent token has features 'is_caps', then the features for the
+               current token will also include 'next_is_caps'.
+    Returns:
+    - A list of dicts, one per token, containing the features for that token.
+    - A numpy array, one per token, containing the NER tag for that token.
+    """
+    ###TODO
+    pass
+
+
+def confusion(true_labels, pred_labels):
+	"""
+	Create a confusion matrix, where cell (i,j)
+	is the number of tokens with true label i and predicted label j.
+
+	Params:
+	  true_labels....numpy array of true NER labels, one per token
+	  pred_labels....numpy array of predicted NER labels, one per token
+	Returns:
+	A Pandas DataFrame (http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html)
+	See Log.txt for an example.
+	"""
+	###TODO
+	pass
+
+def evaluate(confusion_matrix):
+	"""
+	Compute precision, recall, f1 for each NER label.
+	The table should be sorted in ascending order of label name.
+	If the denominator needed for any computation is 0,
+	use 0 as the result.  (E.g., replace NaNs with 0s).
+
+	NOTE: you should implement this on your own, not using
+	      any external libraries (other than Pandas for creating 
+	      the output.)
+	Params:
+	  confusion_matrix...output of confusion function above.
+	Returns:
+	  A Pandas DataFrame. See Log.txt for an example.
+	"""
+	###TODO
+	pass
+
+def average_f1s(evaluation_matrix):
+	"""
+	Returns:
+	The average F1 score for all NER tags, 
+	EXCLUDING the O tag.
+	"""
+	###TODO
+	pass
+
+def evaluate_combinations(train_data, test_data):
+	"""
+	Run 16 different settings of the classifier, 
+	corresponding to the 16 different assignments to the
+	parameters to make_feature_dicts:
+	caps, pos, chunk, context
+	That is, for one setting, we'll use
+	token=True, caps=False, pos=False, chunk=False, context=False
+	and for the next setting we'll use
+	token=True, caps=False, pos=False, chunk=False, context=True
+
+	For each setting, create the feature vectors for the training
+	and testing set, fit a LogisticRegression classifier, and compute
+	the average f1 (using the above functions).
+
+	Returns:
+	A Pandas DataFrame containing the F1 score for each setting, 
+	along with the total number of parameters in the resulting
+	classifier. This should be sorted in descending order of F1.
+	(See Log.txt).
+
+	Note1: You may find itertools.product helpful for iterating over
+	combinations.
+
+	Note2: You may find it helpful to read the main method to see
+	how to run the full analysis pipeline.
+	"""
+	###TODO
+	pass
+
+
+if __name__ == '__main__':
+	"""
+        You'll have to modify this based on the instructions.
+	"""
+    pass
